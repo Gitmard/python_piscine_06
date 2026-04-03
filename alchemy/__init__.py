8ea101a4d55efd1429a73445909366b4572f94fc
smenard <smenard@student.42lyon.fr>
2026-04-03 15:43:11 +0200

    feat(Part II): ft_distillation_1

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["create_earth", "create_air", "healing_potion", "strength_potion"]
+__all__ = ["create_earth", "create_air", "heal", "strength_potion"]
 
 from alchemy.elements import create_air
-from alchemy.potions import healing_potion, strength_potion
+from alchemy.potions import healing_potion as heal, strength_potion
