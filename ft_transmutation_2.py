0c263d3f933cef81c5c1ed79c822deff3b34142e
smenard <smenard@student.42lyon.fr>
2026-04-03 20:59:42 +0200

    fix(Part III): Use alchemy to call lead_to_gold

--- ft_transmutation_2.py
+++ ft_transmutation_2.py
@@ -5,4 +5,4 @@ import alchemy
 if __name__ == "__main__":
     print("=== Transmutation 2 ===")
     print("Import alchemy module only")
-    print(f"Testing lead to gold: {alchemy.transmutation.lead_to_gold()}")
+    print(f"Testing lead to gold: {alchemy.lead_to_gold()}")
