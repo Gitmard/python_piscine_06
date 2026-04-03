68803c068ba2a37fcacf96f8a7d534c43db1dd3b
smenard <smenard@student.42lyon.fr>
2026-04-03 15:37:35 +0200

    feat(Part II): Potions

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ["create_earth", "create_air"]
+__all__ = ["create_earth", "create_air", "healing_potion", "strength_potion"]
 
 from alchemy.elements import create_air
+from alchemy.potions import healing_potion, strength_potion
